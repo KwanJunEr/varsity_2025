1a5cd4a4625bcf403a3ad0b4df9ad662424bece0
Sean Hoe Kai Zher <102246906+Seann2003@users.noreply.github.com>
2025-03-25 17:18:24 +0800

    Created game scene

---
+++ data/aiAgentsData.ts
@@ -0,0 +1,23 @@
+export const spriteImages = [
+  {
+    id: "ai-agent-1",
+    src: "/ai-agent-1.png",
+    tileX: 6,
+    tileY: 5,
+    speed: 1,
+  },
+  {
+    id: "ai-agent-2",
+    src: "/ai-agent-2.png",
+    tileX: 4,
+    tileY: 2,
+    speed: 1,
+  },
+  {
+    id: "ai-agent-3",
+    src: "/ai-agent-3.png",
+    tileX: 3,
+    tileY: 5,
+    speed: 1,
+  },
+];
