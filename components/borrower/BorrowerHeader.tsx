e3ae7480d4a3653bb3a898ea7f67bf0b2da49be4
KwanJunEr <jonastheitguy@gmail.com>
2025-03-26 11:51:12 +0800

    completed microloan page

---
+++ components/borrower/BorrowerHeader.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="bg-white shadow-md fixed top-0 left-0 right-0 z-50">
+      <div className="flex justify-between h-16 items-center px-10">
+        <Link href = {"/dashboard"}>
+        <h2 className="font-extrabold text-2xl">LendSync</h2>
+        </Link >
+        <div className="flex space-x-8">
+          <Link
+            href="/borrower"
+            className={`inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
+              pathname === "/borrower"
+                ? "border-indigo-500 text-gray-900"
+                : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
+            }`}
+          >
+            Dashboard
+          </Link>
+          <Link
+            href="/marketplace"
+            className={`inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
+              pathname === "/marketplace"
+                ? "border-indigo-500 text-gray-900"
+                : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
+            }`}
+          >
+            Marketplace
+          </Link>
+          <Link
+            href="/pet"
+            className={`inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium ${
+              pathname === "/pet"
+                ? "border-indigo-500 text-gray-900"
+                : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
+            }`}
+          >
+            Pet
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
